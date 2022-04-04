03b575a56b8b4f0e9a3821084f069547aa1b2fd9
Assim <assim.s@techversantinfo.com>
2022-04-04 10:54:09 +0530

    final

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ let id = (id) => document.getElementById(id);
 let classes = (classes) => document.getElementsByClassName(classes);
 
 let fname = id("fname"),
+    lname=id("lname"),
+    mobile=id("mobile"),
+    address=id("address")
     email = id("email"),
     password = id("password"),
+
     form= id("form"),
 
     errorMsg = classes("error"),
@@ -18,7 +22,8 @@ form.addEventListener("submit", (e) => {
     engine(lname, 1, "Last name cannot be blank")
     engine(email, 2, "Email cannot be blank");
     engine(mobile, 3, "Mobile number cannot be blank")
-    engine(password, 11, "Password cannot be blank");
+    engine(address, 4, "Address cannot be blank")
+    engine(password, 5, "Password cannot be blank. Must contain atleast one spaecial character");
 });
 
 let engine = (id, serial, message) => {
@@ -26,15 +31,13 @@ let engine = (id, serial, message) => {
     if (id.value.trim() === "") {
     errorMsg[serial].innerHTML = message;
     id.style.border = "2px solid red";
-      
-    // icons
+
     failureIcon[serial].style.opacity = "1";
     successIcon[serial].style.opacity = "0";
     } else {
       errorMsg[serial].innerHTML = "";
       id.style.border = "2px solid green";
       
-      // icons
       failureIcon[serial].style.opacity = "0";
       successIcon[serial].style.opacity = "1";
     }
